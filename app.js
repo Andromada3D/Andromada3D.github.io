af8ea0260a3bd9610f5bd6b2982b2bb66951647c
Michel <97351775+MichelosRey@users.noreply.github.com>
2022-05-24 13:56:07 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function sol(){
-    document.getElementById("sol").style = "display:none;";
-    document.getElementById("luna").style = "display:block;";
-}
-function luna(){
-    document.getElementById("luna").style = "display:none;";
-    document.getElementById("sol").style = "display:block;";
-}
